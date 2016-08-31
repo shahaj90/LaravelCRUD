883befe7b6302dc825b964d62be4ef190fb6ec45
shahaj90 <gshahaj@gmail.com>
2016-08-31 20:33:24 +0600

    iiii

---
+++ public/app/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('employeeRecords', [])
+        .constant('API_URL', 'http://localhost/laravelCRUD/public/api/v1/');
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+//Route::get('/api/v1/employees/{id?}', 'Employees@index');
+//Route::post('/api/v1/employees', 'Employees@store');
+//Route::post('/api/v1/employees/{id}', 'Employees@update');
+//Route::delete('/api/v1/employees/{id}', 'Employees@destroy');
