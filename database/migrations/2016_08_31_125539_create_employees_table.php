bdb285de28d55912df9717a1905348a8a57ece3e
shahaj90 <gshahaj@gmail.com>
2016-08-31 19:09:52 +0600

    migration

--- database/migrations/2016_08_31_125539_create_employees_table.php
+++ database/migrations/2016_08_31_125539_create_employees_table.php
@@ -4,16 +4,22 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEmployeesTable extends Migration
-{
+class CreateEmployeesTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
+    public function up() {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->string('contact_number');
+            $table->string('position');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -21,8 +27,8 @@ class CreateEmployeesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        //
+    public function down() {
+        Schema::drop('employees');
     }
+
 }
